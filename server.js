a0b4d6235f109353cf7d04efab42cc80262660ef
Kelvin Moi <kelvin.mai002@gmail.com>
2018-01-12 15:18:56 -0600

    Added dependencies

--- server.js
+++ server.js
@@ -1,8 +1,25 @@
+// enviornment variables
+require('dotenv').config();
+const port = process.env.PORT,
+mongoDB = process.env.DATABASE_URL;
+
+// dependencies
 const express = require('express'),
+mongoose = require('mongoose'),
+cors = require('cors'),
+bodyParser = require('body-parser'),
 server = express();
 
-port = process.env.PORT || 3000;
+// config
+mongoose.connect(mongoDB, { useMongoClient: true });
+mongoose.Promise = global.Promise;
+let db = mongoose.connection;
+db.on('error', console.error.bind(console, 'MongoDB connection error: '));
+server.use(cors());
+server.use(bodyParser.json());
+server.use(bodyParser.urlencoded({extended:true}));
 
-server.get('/', (req, res) => res.send('Testing'));
+
+server.get('/', (req, res) => res.json({ message: 'Welcome'}));
 
 server.listen(port, () => console.log(`Server started on port ${port}`));
